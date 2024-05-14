c066421da52f3bb7b01f74688dad831b434594c1
Jonah Poczobutt <jonahpocz@gmail.com>
2024-05-13 23:51:38 -0400

    optimization for dif sim

--- TunaSims.py
+++ TunaSims.py
@@ -21,8 +21,135 @@ def middle(element,lower,upper):
             return upper
     else:
         return lower
-
+    
 def tuna_dif_distance(query,
+                    target,
+                    a = 0,
+                    b = 0,
+                    c = 1,
+                    d = -np.inf,
+                    e = np.inf,
+                    f = 0,
+                    g = 0,
+                    h = 1,
+                    i = -np.inf,
+                    j = np.inf,
+                    k = 0,
+                    l = 0,
+                    m = 1,
+                    n = -np.inf,
+                    o = np.inf,
+                    p = 0,
+                    q = 0,
+                    r = 1,
+                    s = -np.inf,
+                    t = np.inf,
+                    u = 0,
+                    v = 0,
+                    w = 1,
+                    x = -np.inf,
+                    y = np.inf,
+                    z = 0,
+                    a_ = 0,
+                    b_ = 1,
+                    c_ = -np.inf,
+                    d_ = np.inf,
+                    ):
+    """
+    function of individual disagreements, sum_disagreement and length 
+    constant and exponential for each
+    knock-in for each
+    knock-out for each
+    Betas for each
+    interactions for each
+    """
+    dif = np.abs(query-target)
+
+    #array to hold terms
+    terms = np.zeros(6)
+    
+    #check who needs to be evaluated
+    if a != 0:
+        total_disagreement = np.sum(dif)+1e-20
+        terms[0] += middle(a*(total_disagreement+b)**c,d,e)
+    
+    if f != 0:
+        ind_disagreements = np.sum((dif+1e-20)**h)
+        terms[1] += middle(f*(ind_disagreements+g),i,j)
+
+    if k != 0:
+        disagreement_length = len(dif)
+        terms[2] += middle(k*(disagreement_length+l)**m,n,o)
+    
+    if p != 0:
+        terms[3] += middle(p*(total_disagreement*ind_disagreements+q)**r,s,t) 
+
+    if u != 0:
+        terms[4] += middle(u*(total_disagreement*disagreement_length+v)**w,x,y)
+
+    if z != 0:
+        terms[5] += middle(z*(ind_disagreements*disagreement_length+a_)**b_,c_,d_)
+
+    return np.sum(terms)
+
+    
+def tuna_dif_distance_old(query,
+                    target,
+                    a = 0,
+                    b = 0,
+                    c = 1,
+                    d = -np.inf,
+                    e = np.inf,
+                    f = 0,
+                    g = 0,
+                    h = 1,
+                    i = -np.inf,
+                    j = np.inf,
+                    k = 0,
+                    l = 0,
+                    m = 1,
+                    n = -np.inf,
+                    o = np.inf,
+                    p = 0,
+                    q = 0,
+                    r = 1,
+                    s = -np.inf,
+                    t = np.inf,
+                    u = 0,
+                    v = 0,
+                    w = 1,
+                    x = -np.inf,
+                    y = np.inf,
+                    z = 0,
+                    a_ = 0,
+                    b_ = 1,
+                    c_ = -np.inf,
+                    d_ = np.inf,
+                    ):
+    """
+    function of individual disagreements, sum_disagreement and length 
+    constant and exponential for each
+    knock-in for each
+    knock-out for each
+    Betas for each
+    interactions for each
+    """
+    dif = np.abs(query-target)
+    
+    #check who needs to be evaluated
+    total_disagreement = np.sum(dif)+1e-20
+    ind_disagreements = np.sum((dif+1e-20)**h)
+    disagreement_length = len(dif)
+
+    individuals = middle(a*(total_disagreement+b)**c,d,e) + middle(f*(ind_disagreements+g),i,j) + middle(k*(disagreement_length+l)**m,n,o)
+    interactions = middle(p*(total_disagreement*ind_disagreements+q)**r,s,t) \
+                   + middle(u*(total_disagreement*disagreement_length+v)**w,x,y) \
+                   + middle(z*(ind_disagreements*disagreement_length+a_)**b_,c_,d_)
+
+    return sigmoid(individuals + interactions)
+
+
+def tuna_dif_distance_double_old(query,
                     target,
                     a = 0,
                     b = 1,
