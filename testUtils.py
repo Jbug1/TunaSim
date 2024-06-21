9e529994ca2a9cfcd6e32e73423cc2ef6c94707b
Jonah Poczobutt <jonahpocz@gmail.com>
2024-06-21 14:35:43 -0700

    added correlation based metric selection for boosting models

--- testUtils.py
+++ testUtils.py
@@ -6,7 +6,44 @@ import tools
 import numpy as np
 import pandas as pd
 from sklearn.metrics import roc_auc_score as auc
+import itertools
+import math
 
+
+def get_least_corr_and_control(dataset, num):
+
+    lowest_seen = 1
+    best_group=None
+
+    #choose every combo for given number
+    for combo in itertools.combinations(range(len(dataset.columns)),r=num):
+
+        corr = 0
+
+        if set(combo) == set([3,4,0,1]):
+            print('seen')
+        for i in combo:
+            for j in combo:
+
+                if i>j:
+                    corr += (np.corrcoef(dataset[dataset.columns[i]],dataset[dataset.columns[j]]))[0,1]/math.comb(num,2)
+
+        if corr < lowest_seen:
+            lowest_seen = corr
+            best_group = combo
+
+    rand_control = np.random.choice(list(range(dataset.shape[1])),replace=False,size=num)
+    corr=0
+    rand_control = [0,1,3,4]
+    print(rand_control)
+    for i in rand_control:
+            for j in rand_control:
+
+                if i>j:
+                    corr += (np.corrcoef(dataset[dataset.columns[i]],dataset[dataset.columns[j]]))[0,1]/math.comb(num,2)
+
+    return((best_group,lowest_seen),(rand_control,corr))
+                    
 
 def dict_combine(dict1,dict2):
 
