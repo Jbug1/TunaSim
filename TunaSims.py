c2891c493e08cb5db49d00b12d86cf482cbd21d1
Jonah Poczobutt <jonahpocz@gmail.com>
2025-02-05 21:01:49 -0500

    weight contibutions triggers implemented

--- TunaSims.py
+++ TunaSims.py
@@ -1,9 +1,12 @@
 import numpy as np
 import scipy
 import tools_fast
+from dataclasses import dataclass
+
 
 class TunaSim:
 
+    @staticmethod
     def tuna_weight_intensity(spectrum,
                           fixed_exp = 0,
                           mz_exp = 0,
@@ -16,6 +19,7 @@ class TunaSim:
         
         return np.power(intensities, offset + mz_power_array + fixed_exp + scipy.stats.entropy(intensities) ** entropy_exp)
 
+    @staticmethod
     def smooth_reweight(array,
                         intercept,
                         a,
@@ -27,128 +31,93 @@ class TunaSim:
         
         return intercept + a * array + b * np.power(array,2) + c * np.power(array,3)
         
+    @staticmethod
     def sigmoid(z):
     
         return 1/(1 + np.exp(-z))
     
-    
+@dataclass
 class TunaSmoothSim(TunaSim):
+                
+    query_mz_int: float = 0
+    query_mz_a: float = 0
+    query_mz_b: float = 0
+    query_mz_c: float = 0
+    target_mz_int: float = 0
+    target_mz_a: float = 0
+    target_mz_b: float = 0
+    target_mz_c: float = 0
+    query_mz_offset_int: float = 0
+    query_mz_offset_a: float = 0
+    query_mz_offset_b: float = 0
+    query_mz_offset_c: float = 0
+    target_mz_offset_int: float = 0
+    target_mz_offset_a: float = 0
+    target_mz_offset_b: float = 0
+    target_mz_offset_c: float = 0
+    query_intensity_int: float = 0
+    query_intensity_a: float = 0
+    query_intensity_b: float = 0
+    query_intensity_c: float = 0
+    target_intensity_int: float = 0
+    target_intensity_a: float = 0
+    target_intensity_b: float = 0
+    target_intensity_c: float = 0
+    query_normalized_intensity_int: float = 0
+    query_normalized_intensity_a: float = 0
+    query_normalized_intensity_b: float = 0
+    query_normalized_intensity_c: float = 0
+    target_normalized_intensity_int: float = 0
+    target_normalized_intensity_a: float = 0
+    target_normalized_intensity_b: float = 0
+    target_normalized_intensity_c: float = 0
+    query_entropy_int: float = 0
+    query_entropy_a: float = 0
+    query_entropy_b: float = 0
+    query_entropy_c: float = 0
+    target_entropy_int: float = 0
+    target_entropy_a: float = 0
+    target_entropy_b: float = 0
+    target_entropy_c: float = 0
+    dif_a: float = 0
+    dif_b: float= 1
+    mult_a: float = 0
+    mult_b: float = 1
+    add_norm_a: float= 0
+    add_norm_b: float = 1
+    mult_norm_a: float = 0
+    mult_norm_b: float = 1
+    unnormed: float = 0
+    collapsed: float = 0
+    expanded: float = 0
+    match_tolerance: float = 0.05
+    sim_flip: float= False
+    zero_clip: float = True
+    restandardize: float = True
+    sig_factor: float = 1
 
-    def __init__(self,
-                 query_raw_mz_int = 0,
-                query_raw_mz_a = 0,
-                query_raw_mz_b = 0,
-                query_raw_mz_c = 0,
-                target_raw_mz_int = 0,
-                target_raw_mz_a = 0,
-                target_raw_mz_b = 0,
-                target_raw_mz_c = 0,
-                query_prop_mz_int = 0,
-                query_prop_mz_a = 0,
-                query_prop_mz_b = 0,
-                query_prop_mz_c = 0,
-                target_prop_mz_int = 0,
-                target_prop_mz_a = 0,
-                target_prop_mz_b = 0,
-                target_prop_mz_c = 0,
-                query_intensity_mz_int = 0,
-                query_intensity_mz_a = 0,
-                query_intensity_mz_b = 0,
-                query_intensity_mz_c = 0,
-                target_intensity_int = 0,
-                target_intensity_a = 0,
-                target_intensity_b = 0,
-                target_intensity_c = 0,
-                query_norm_int_int = 0,
-                query_norm_int_mz_a = 0,
-                query_norm_int_mz_b = 0,
-                query_norm_int_mz_c = 0,
-                target_norm_int_int = 0,
-                target_norm_int_a = 0,
-                target_norm_int_b = 0,
-                target_norm_int_c = 0,
-                query_entropy_int = 0,
-                query_entropy_a = 0,
-                query_entropy_b = 0,
-                query_entropy_c = 0,
-                target_entropy_int = 0,
-                target_entropy_a = 0,
-                target_entropy_b = 0,
-                target_entropy_c = 0,
-                dif_a = 0,
-                dif_b = 1,
-                mult_a = 0,
-                mult_b = 1,
-                add_norm_a = 0,
-                add_norm_b = 1,
-                mult_norm_a = 0,
-                mult_norm_b = 1,
-                unnormed = 0,
-                collapsed = 0,
-                expanded = 0,
-                match_tolerance = 0.05,
-                sim_flip = False,
-                zero_clip = True,
-                restandardize = True
-                ):
-        
-        super.__init__()
-        
-        self.query_raw_mz_int  = query_raw_mz_int 
-        self.query_raw_mz_a  = query_raw_mz_a 
-        self.query_raw_mz_b  = query_raw_mz_b 
-        self.query_raw_mz_c  = query_raw_mz_c 
-        self.target_raw_mz_int  = target_raw_mz_int 
-        self.target_raw_mz_a  = target_raw_mz_a 
-        self.target_raw_mz_b  = target_raw_mz_b 
-        self.target_raw_mz_c  = target_raw_mz_c 
-        self.query_prop_mz_int  = query_prop_mz_int 
-        self.query_prop_mz_a  = query_prop_mz_a 
-        self.query_prop_mz_b  = query_prop_mz_b 
-        self.query_prop_mz_c  = query_prop_mz_c 
-        self.target_prop_mz_int  = target_prop_mz_int 
-        self.target_prop_mz_a  = target_prop_mz_a 
-        self.target_prop_mz_b  = target_prop_mz_b 
-        self.target_prop_mz_c  = target_prop_mz_c 
-        self.query_intensity_mz_int  = query_intensity_mz_int 
-        self.query_intensity_mz_a  = query_intensity_mz_a 
-        self.query_intensity_mz_b  = query_intensity_mz_b 
-        self.query_intensity_mz_c  = query_intensity_mz_c 
-        self.target_intensity_int  = target_intensity_int 
-        self.target_intensity_a  = target_intensity_a 
-        self.target_intensity_b  = target_intensity_b 
-        self.target_intensity_c  = target_intensity_c 
-        self.query_norm_int_int  = query_norm_int_int 
-        self.query_norm_int_mz_a  = query_norm_int_mz_a 
-        self.query_norm_int_mz_b  = query_norm_int_mz_b 
-        self.query_norm_int_mz_c  = query_norm_int_mz_c 
-        self.target_norm_int_int  = target_norm_int_int 
-        self.target_norm_int_a  = target_norm_int_a 
-        self.target_norm_int_b  = target_norm_int_b 
-        self.target_norm_int_c  = target_norm_int_c 
-        self.query_entropy_int  = query_entropy_int 
-        self.query_entropy_a  = query_entropy_a 
-        self.query_entropy_b  = query_entropy_b 
-        self.query_entropy_c  = query_entropy_c 
-        self.target_entropy_int  = target_entropy_int 
-        self.target_entropy_a  = target_entropy_a 
-        self.target_entropy_b  = target_entropy_b 
-        self.target_entropy_c  = target_entropy_c 
-        self.dif_a  = dif_a 
-        self.dif_b  = dif_b 
-        self.mult_a  = mult_a 
-        self.mult_b  = mult_b 
-        self.add_norm_a  = add_norm_a 
-        self.add_norm_b  = add_norm_b 
-        self.mult_norm_a  = mult_norm_a 
-        self.mult_norm_b  = mult_norm_b 
-        self.unnormed  = unnormed 
-        self.collapsed  = collapsed 
-        self.expanded  = expanded 
-        self.match_tolerance  = match_tolerance 
-        self.sim_flip  = sim_flip 
-        self.zero_clip = zero_clip
-        self.restandardize = restandardize
+    def __post_init__(self):
+
+        self.set_weight_triggers()
+
+    def set_weight_triggers(self):
+        ''' 
+        look at which parameters are set to determine which calculations are necessary
+        '''
+        triggers = ['mz',
+                    'mz_offset',
+                    'intensity',
+                    'normalized_intensity',
+                    'entropy'
+                    ]
+        #set initial state to False, if some input is non-zero, then flip it to True
+        for trigger in triggers:
+            setattr(self, trigger, False)
+            for side in ['query','target']:
+                for variable in ['int','a','b','c']:
+
+                    if getattr(self, f'{side}_{trigger}_{variable}') != 0:
+                        setattr(self, trigger, True)
 
     def predict(self,
                 query,
@@ -208,9 +177,9 @@ class TunaSmoothSim(TunaSim):
 
         #some metrics are expressed as sim measures
         if self.sim_flip:
-            return self.sigmoid(unnormed_term + collapsed_term + expanded_term)
+            return self.sigmoid(self.sig_factor *(unnormed_term + collapsed_term + expanded_term))
         else:
-            return 1 - self.sigmoid(unnormed_term + collapsed_term + expanded_term)
+            return 1 - self.sigmoid(self.sig_factor *(unnormed_term + collapsed_term + expanded_term))
 
 
     def smooth_reweight(self,
@@ -223,47 +192,47 @@ class TunaSmoothSim(TunaSim):
         '''
 
         #get reweight based on raw mz values
-        if self.mz_weights:
-            self.query_mz_weights = tools.tuna_smooth_weight(query[:,0], 
-                                                                self.query_raw_mz_int,
-                                                                self.query_raw_mz_a,
-                                                                self.query_raw_mz_b,
-                                                                self.query_raw_mz_c,
+        if self.mz:
+            self.query_mz_weights = self.tuna_smooth_weight(query[:,0], 
+                                                                self.query_mz_int,
+                                                                self.query_mz_a,
+                                                                self.query_mz_b,
+                                                                self.query_mz_c,
                                                             )
                             
-            self.target_mz_weights = tools.tuna_smooth_weight(target[:,0], 
-                                                                self.target_raw_mz_int,
-                                                                self.target_raw_mz_a,
-                                                                self.target_raw_mz_b,
-                                                                self.target_raw_mz_c,
+            self.target_mz_weights = self.tuna_smooth_weight(target[:,0], 
+                                                                self.target_mz_int,
+                                                                self.target_mz_a,
+                                                                self.target_mz_b,
+                                                                self.target_mz_c,
                                                             )
         
         #grab weights for spectra as precursor offset
-        if self.query_mz_offset_weights:
-            self.query_mz_offset_weights = tools.tuna_smooth_weight(query[:,0] - prec_query, 
-                                                                    self.query_prop_mz_int,
-                                                                    self.query_prop_mz_a,
-                                                                    self.query_prop_mz_b,
-                                                                    self.query_prop_mz_c,
+        if self.mz_offset:
+            self.query_mz_offset_weights = self.tuna_smooth_weight(query[:,0] - prec_query, 
+                                                                    self.query_mz_offset_int,
+                                                                    self.query_mz_offset_a,
+                                                                    self.query_mz_offset_b,
+                                                                    self.query_mz_offset_c,
                                                                 )
             
-            self.target_mz_offset_weights = tools.tuna_smooth_weight(target[:,0] - prec_target, 
-                                                                    self.target_prop_mz_int, 
-                                                                    self.target_prop_mz_a,
-                                                                    self.target_prop_mz_b,
-                                                                    self.target_prop_mz_c,
+            self.target_mz_offset_weights = self.tuna_smooth_weight(target[:,0] - prec_target, 
+                                                                    self.target_mz_offset_int, 
+                                                                    self.target_mz_offset_a,
+                                                                    self.target_mz_offset_b,
+                                                                    self.target_mz_offset_c,
                                                                 )
         
         #as a function of intensities
-        if self.intensity_weights:
-            self.query_int_weights = tools.tuna_smooth_weight(query[:,1], 
-                                                                self.query_intensity_mz_int,
-                                                                self.query_intensity_mz_a,
-                                                                self.query_intensity_mz_b,
-                                                                self.query_intensity_mz_c,
+        if self.intensity:
+            self.query_int_weights = self.tuna_smooth_weight(query[:,1], 
+                                                                self.query_intensity_int,
+                                                                self.query_intensity_a,
+                                                                self.query_intensity_b,
+                                                                self.query_intensity_c,
                                                             )
             
-            self.target_int_weights = tools.tuna_smooth_weight(target[:,1],
+            self.target_int_weights = self.tuna_smooth_weight(target[:,1],
                                                                 self.target_intensity_int, 
                                                                 self.target_intensity_a,
                                                                 self.target_intensity_b,
@@ -271,31 +240,31 @@ class TunaSmoothSim(TunaSim):
                                                             )
         
         #as a funciton of normalized intensities
-        if self.normalized_intensity_weights:
-            self.query_norm_int_weights = tools.tuna_smooth_weight(query[:,1] /np.sum(query[:,1]), 
-                                                            self.query_norm_int_int,
-                                                            self.query_norm_int_mz_a,
-                                                            self.query_norm_int_mz_b,
-                                                            self.query_norm_int_mz_c,
+        if self.normalized_intensity:
+            self.query_norm_int_weights = self.tuna_smooth_weight(query[:,1] /np.sum(query[:,1]), 
+                                                            self.query_normalized_intensity_int,
+                                                            self.query_normalized_intensity_a,
+                                                            self.query_normalized_intensity_b,
+                                                            self.query_normalized_intensity_c,
                                                             )
             
-            self.target_norm_int_weights = tools.tuna_smooth_weight(target[:,1] / np.sum(target[:,1]), 
-                                                    self.target_norm_int_int,
-                                                    self.target_norm_int_a,
-                                                    self.target_norm_int_b,
-                                                    self.target_norm_int_c,
+            self.target_norm_int_weights = self.tuna_smooth_weight(target[:,1] / np.sum(target[:,1]), 
+                                                    self.target_normalized_intensity_int,
+                                                    self.target_normalized_intensity_a,
+                                                    self.target_normalized_intensity_b,
+                                                    self.target_normalized_intensity_c,
                                                     )
         
 
-        if self.entropy_weights:
-            self.query_entropy_weights = tools.tuna_smooth_weight(np.zeros(len(query)) + scipy.stats.entropy(query[:,1]), 
+        if self.entropy:
+            self.query_entropy_weights = self.tuna_smooth_weight(np.zeros(len(query)) + scipy.stats.entropy(query[:,1]), 
                                                     self.query_entropy_int,
                                                     self.query_entropy_a,
                                                     self.query_entropy_b,
                                                     self.query_entropy_c,
                                                     )
             
-            self.target_entropy_weights = tools.tuna_smooth_weight(np.zeros(len(target)) + scipy.stats.entropy(target[:,1]), 
+            self.target_entropy_weights = self.tuna_smooth_weight(np.zeros(len(target)) + scipy.stats.entropy(target[:,1]), 
                                                     self.target_entropy_int,
                                                     self.target_entropy_a,
                                                     self.target_entropy_b,
