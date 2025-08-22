c923437914d2d952a484da0d390f80a208b3366d
Jonah Poczobutt <jonahpocz@gmail.com>
2025-08-21 21:46:15 -0400

    adding entropy feature to query layer

--- oldMetrics.py
+++ oldMetrics.py
@@ -1,9 +1,11 @@
 # old similarity measures
 import scipy
 import numpy as np
+from numba import njit
+from joblib import Parallel, delayed
 
 
-def _weight_intensity_by_entropy(x):
+def _weight_intensity_by_entropy(x, entropy_x):
     WEIGHT_START = 0.25
     ENTROPY_CUTOFF = 3
     weight_slope = (1 - WEIGHT_START) / ENTROPY_CUTOFF
@@ -17,6 +19,7 @@ def _weight_intensity_by_entropy(x):
             x = x / x_sum
     return x
 
+@njit
 def harmonic_mean_distance(p, q):
     r"""
     Harmonic mean distance:
@@ -25,10 +28,10 @@ def harmonic_mean_distance(p, q):
 
         1-2\sum(\frac{P_{i}Q_{i}}{P_{i}+Q_{i}})
     """
-    p = _weight_intensity_by_entropy(p)
-    q = _weight_intensity_by_entropy(q)
+
     return 2 * np.sum(p * q / (p + q))
 
+@njit
 def lorentzian_distance(p, q):
     r"""
     Lorentzian distance:
@@ -37,10 +40,10 @@ def lorentzian_distance(p, q):
 
         \sum{\ln(1+|P_i-Q_i|)}
     """
-    p = _weight_intensity_by_entropy(p)
-    q = _weight_intensity_by_entropy(q)
+
     return 1 - np.sum(np.log(1 + np.abs(p - q)))
 
+@njit
 def matusita_distance(p, q):
     r"""
     Matusita distance:
@@ -49,10 +52,10 @@ def matusita_distance(p, q):
 
         \sqrt{\sum(\sqrt{P_{i}}-\sqrt{Q_{i}})^2}
     """
-    p = _weight_intensity_by_entropy(p)
-    q = _weight_intensity_by_entropy(q)
+
     return 1- np.sum(np.power(np.sqrt(p) - np.sqrt(q), 2))
 
+@njit
 def probabilistic_symmetric_chi_squared_distance(p, q):
     r"""
     Probabilistic symmetric χ2 distance:
@@ -61,9 +64,9 @@ def probabilistic_symmetric_chi_squared_distance(p, q):
 
         \frac{1}{2} \times \sum\frac{(P_{i}-Q_{i}\ )^2}{P_{i}+Q_{i}\ }
     """
-    p = _weight_intensity_by_entropy(p)
-    q = _weight_intensity_by_entropy(q)
+ 
     return 1- (1 / 2 * np.sum(np.power(p - q, 2) / (p + q)))
+
 
 def entropy_distance(p, q):
     r"""
@@ -73,8 +76,7 @@ def entropy_distance(p, q):
 
         -\frac{2\times S_{PQ}-S_P-S_Q} {ln(4)}, S_I=\sum_{i} {I_i ln(I_i)}
     """
-    p = _weight_intensity_by_entropy(p)
-    q = _weight_intensity_by_entropy(q)
+   
     merged = p + q
     entropy_increase = 2 * \
                        scipy.stats.entropy(merged) - scipy.stats.entropy(p) - \
@@ -82,6 +84,7 @@ def entropy_distance(p, q):
     
     return 1 - entropy_increase
 
+@njit
 def dot_product_distance(p, q):
     r"""
     Dot product distance:
@@ -90,9 +93,21 @@ def dot_product_distance(p, q):
 
         1 - \sqrt{\frac{(\sum{Q_iP_i})^2}{\sum{Q_i^2\sum P_i^2}}}
     """
-    p = _weight_intensity_by_entropy(p)
-    q = _weight_intensity_by_entropy(q)    
+    
     score = np.power(np.sum(q * p), 2) / (
         np.sum(np.power(q, 2)) * np.sum(np.power(p, 2))
     )
     return np.sqrt(score)
+
+def evaluate_metrics(queries, targets):
+    """ 
+    Evaluates this set of old measures for a collection of queries and targets
+    """
+
+    metrics = [entropy_distance, ]
+    #unweighted first
+    for query, target in zip(queries, targets):
+
+        
+
+
